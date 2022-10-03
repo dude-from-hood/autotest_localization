abece867e98fb5813ddf6081df155f7d6ac32430
Vladimir Kondrashov <vova.kondrashov1993@gmail.com>
2022-10-03 19:39:00 +0800

    add assert

--- test_items.py
+++ test_items.py
@@ -9,8 +9,9 @@ def test_content_should_display_by_chosen_language(browser):
     browser.get(
         "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/")
     browser.implicitly_wait(1)
-    browser.find_element(By.CSS_SELECTOR, "#login_link")
+    browser.find_elements(By.CSS_SELECTOR, "#login_link")
     buttonBasket = browser.find_element(
         By.CSS_SELECTOR, "[class~='btn-add-to-basket']")
+    assert buttonBasket, 'Селектор кнопки не найден'
     buttonBasket.click()
     time.sleep(3)
